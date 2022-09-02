8efa470140695946326bc474051e5ca40e701793
Kamlesh Mohar <rahulpnd350@gmail.com>
2022-09-02 14:44:44 +0530

    test

--- src/home/instafeed/instafeed.js
+++ src/home/instafeed/instafeed.js
@@ -2,6 +2,8 @@ import { Component } from 'react'
 import insta from './images/insta.png';
 import AOS from 'aos';
 import "aos/dist/aos.css";
+import InstagramFeed from "react-ig-feed";
+
 
 
 export class InstaFeed extends Component {
@@ -12,7 +14,13 @@ export class InstaFeed extends Component {
   }
   render() {
     return (
+      <>
       <div data-aos={"fade-up"}><img src={insta}/> </div>
+      <InstagramFeed
+        token="IGQVJWSEtmbTlhYXdaaGNaRUNPbmotLXpfOGlaU0x4ZAHJMb094MEZApU1h4dF85RDZADejhhaDJ2TnpIV29idWhyTEoyMjRjWk5FbXI5OUx5V19qcWJRdjMzcDl1SkNZAUnVVR3NCOEVGN0JoeHVGSkc3bgZDZD"
+        counter="6"
+      />
+      </>
     )
   }
 }
